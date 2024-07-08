3a40d6848d93eae6ff7eebfaa0f31cc1a977eb0d
spearson1234 <148724094+spearson1234@users.noreply.github.com>
2024-07-08 22:06:55 +0100

    Add files via upload

---
+++ .github/workflows/app.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAXAZGrw_EAHpELQUPrU-c9Um3uyYN6wCw",
+     authDomain: "signup-98a81.firebaseapp.com",
+     databaseURL: "https://signup-98a81-default-rtdb.firebaseio.com",
+     projectId: "signup-98a81",
+     storageBucket: "signup-98a81.appspot.com",
+     messagingSenderId: "557509292895",
+     appId: "1:557509292895:web:6f6e4117bbda34aea93cea"
+ };
+ 
